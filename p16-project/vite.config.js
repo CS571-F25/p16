3b5ce04ce5ba6102ada1ddf5b2d75b2b377222eb
Jason Schwartz <jrschwartz8@wisc.edu>
2025-10-28 18:01:02 -0500

    Created React App

---
+++ p16-project/vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/p16/',
+  build: {
+  outDir: 'docs'
+  }
+})
