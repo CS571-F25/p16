f56f513b5d92a68eb1ade3d10cc6b8a2deef6281
Ayaan Nihal <ayaannihal29@gmail.com>
2025-12-09 17:42:16 -0600

    Added Local Storage

--- src/contexts/FavoriteContext.jsx
+++ src/contexts/FavoriteContext.jsx
@@ -5,6 +5,18 @@ const FavoriteContext = createContext();
 export function FavoriteProvider({ children }) {
   const [favorites, setFavorites] = useState([]);
 
+  // Saving Favorite logic
+  useEffect(() => {
+    const stored = localStorage.getItem("favorites");
+    if (stored) {
+       setFavorites(JSON.parse(stored));
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("favorites", JSON.stringify(favorites));
+  }, [favorites]);
+
   const addFavorite = (shoe) => {
     setFavorites(prev => {
       const exists = prev.some(fav => fav.id === shoe.id);
